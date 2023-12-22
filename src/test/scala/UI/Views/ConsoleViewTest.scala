0a38d8a5fd7a5b83af0fe5d967985fe3dcf1a7a6
musilmag <musilmag@fit.cvut.cz>
2023-12-22 16:54:18 +0100

    Add UI tests

--- src/test/scala/UI/Views/ConsoleViewTest.scala
+++ src/test/scala/UI/Views/ConsoleViewTest.scala
@@ -34,4 +34,95 @@ class ConsoleViewTest extends FunSuite with TimeLimitedTests {
     )
     assert(res.equals(expectedRes))
   }
+
+  test("ConsoleViewTestUnknownCommand") {
+    val input = new StringInput
+    val output = new StringOutput
+    val consoleView = new ConsoleView(input, output)
+
+    input.WriteLine(Seq("--image", "loaderTest2.png", "--output-file", "tmp.txt", "--custom-table", "XIO", "--invert", "--IDK"))
+
+    intercept[Exception] {
+      consoleView.Start()
+    }
+  }
+
+  test("ConsoleViewTestInvalidArgument") {
+    val input = new StringInput
+    val output = new StringOutput
+    val consoleView = new ConsoleView(input, output)
+
+    input.WriteLine(Seq("--image", "loaderTest2.png", "--output-file", "tmp.txt", "--custom-table", "XIO", "--invert", "--flip", "S"))
+
+    intercept[Exception] {
+      consoleView.Start()
+    }
+  }
+
+  test("ConsoleViewTestMissingCommand") {
+    val input = new StringInput
+    val output = new StringOutput
+    val consoleView = new ConsoleView(input, output)
+
+    input.WriteLine(Seq("--output-file", "tmp.txt", "--custom-table", "XIO", "--invert"))
+
+    intercept[Exception] {
+      consoleView.Start()
+    }
+  }
+
+  test("ConsoleViewTestNoFilter") {
+    val input = new StringInput
+    val output = new StringOutput
+    val consoleView = new ConsoleView(input, output)
+
+    input.WriteLine(Seq("--image", "loaderTest2.png", "--output-file", "tmp.txt", "--custom-table", "XIO"))
+
+    consoleView.Start()
+
+    val source = Source.fromFile("tmp.txt")
+    val res = source.getLines.toSeq
+    source.close()
+    new File("tmp.txt").delete()
+
+    val expectedRes = Seq(
+      "OXI",
+      "XOX",
+      "IIO"
+    )
+    assert(res.equals(expectedRes))
+  }
+
+  test("ConsoleViewTestMultipleArguments") {
+    val input = new StringInput
+    val output = new StringOutput
+    val consoleView = new ConsoleView(input, output)
+
+    input.WriteLine(Seq(
+      "--image", "loaderTest2.png",
+      "--output-file", "tmp.txt", "--output-file", "tmp2.txt",
+      "--custom-table", "XIO",
+      "--invert", "--flip", "X", "--scale", "2"))
+
+    consoleView.Start()
+
+    val source1 = Source.fromFile("tmp.txt")
+    val source2 = Source.fromFile("tmp2.txt")
+    val res1 = source1.getLines.toSeq
+    val res2 = source2.getLines.toSeq
+    source1.close()
+    source2.close()
+    new File("tmp.txt").delete()
+    new File("tmp2.txt").delete()
+
+    val expectedRes = Seq(
+      "IIIIXX",
+      "IIIIXX",
+      "OOXXOO",
+      "OOXXOO",
+      "XXOOII",
+      "XXOOII",
+    )
+    assert(res1.equals(expectedRes) && res2.equals(expectedRes))
+  }
 }
