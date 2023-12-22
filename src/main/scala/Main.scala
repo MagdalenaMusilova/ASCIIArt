0c6ac344eb649102bb43ea3d55473f91fc24e4d2
musilmag <musilmag@fit.cvut.cz>
2023-12-22 17:01:20 +0100

    Catch exceptions and print them to output

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,5 +4,13 @@ import IO.{ConsoleInput, ConsoleOutput}
 import UI.Views.ConsoleView
 
 object Main extends App {
-  new ConsoleView(new ConsoleInput(args), new ConsoleOutput).Start()
+  val input = new ConsoleInput(args)
+  val output = new ConsoleOutput
+  try {
+    new ConsoleView(input, output).Start()
+  } catch {
+    case e: Throwable =>
+      output.PrintLn("Failed to convert image")
+      output.PrintLn(s"Error: ${e.getMessage}")
+  }
 }
